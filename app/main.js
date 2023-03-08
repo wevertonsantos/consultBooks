1448e1facfb373ac0df0b60bce8f0ed619d55dc1
Weverton dos Santos <wever-santos@hotmail.com>
2023-03-08 14:22:38 -0300

    inserindo os livros (imagem e titulo) no js

--- app/main.js
+++ app/main.js
@@ -2,8 +2,26 @@ const livros = [];
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosAPI();
 
+const elementoInserirLivros = document.querySelector('#livros')
+
 async function getBuscarLivrosAPI(){
 	const res = await fetch(endpointAPI)
 	const livros = await res.json()
 	console.table(livros)
+	exibirLivros(livros)
+}
+
+function exibirLivros(listaLivros){
+	listaLivros.forEach(livro => {
+		elementoInserirLivros.innerHTML +=
+		`
+			<div class="livro">
+					<img class="img__livro"
+					src="${livro.imagem}">
+					<div class="titulo__livro">
+						<h2>${livro.titulo}</h2>
+					</div>
+			</div>
+		`
+	})
 }
