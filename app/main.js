2f7647dcd3c4c6a943a0bc04d6d08b7bd2638ba6
Weverton dos Santos <wever-santos@hotmail.com>
2023-03-08 15:34:45 -0300

    tirando função exibir livros

--- app/main.js
+++ app/main.js
@@ -7,22 +7,6 @@ const elementoInserirLivros = document.querySelector('#livros')
 async function getBuscarLivrosAPI(){
 	const res = await fetch(endpointAPI)
 	const livros = await res.json()
-	console.table(livros)
-	exibirLivros(livros)
-}
 
-function exibirLivros(listaLivros){
-	listaLivros.forEach(livro => {
-		elementoInserirLivros.innerHTML +=
-		`
-			<div class="livro">
-					<img class="img__livro"
-					src="${livro.imagem}"
-					alt="${livro.alt}">
-					<div class="titulo__livro">
-						<h2>${livro.titulo}</h2>
-					</div>
-			</div>
-		`
-	})
+	exibirLivros(livrosDescontos)
 }
