d5f3c056dd61fc715bc416a21917323f0071a43d
Weverton dos Santos <wever-santos@hotmail.com>
2023-03-08 15:34:29 -0300

    função com forEach e exibindo livros

---
+++ app/exibirLivros.js
@@ -0,0 +1,18 @@
+function exibirLivros(listaLivros){
+	listaLivros.forEach(livro => {
+		elementoInserirLivros.innerHTML +=
+		`
+			<div class="livro">
+					<img class="img__livro"
+					src="${livro.imagem}"
+					alt="${livro.alt}">
+					<div class="titulo__livro">
+						<h2>${livro.titulo}</h2>
+					</div>
+					<div class="tags">
+						<span class="tag">${livro.categoria}</span>
+					</div>
+			</div>
+		`
+	})
+}
